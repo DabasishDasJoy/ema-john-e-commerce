7f198f7093a71e1ab27a2b005779ee8ccb4231aa
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-24 23:48:57 +0600

    card added with style

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const {name, seller, price, ratings} = props.product;
+    const {name, seller, price, ratings, img} = props.product;
     return (
         <div className='product-card'>
-            
+            <img src={img} alt="" />
+            <div className='card-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p className='product-price'>Price: ${price}</p>
+                <div className='production-info'>
+                    <p>Manufacturer: {seller}</p>
+                    <p>Rating: {ratings} stars</p>
+                </div>
+            </div>
+            <button className='btn-add-to-cart'>Add to Cart</button>
         </div>
     );
 };
