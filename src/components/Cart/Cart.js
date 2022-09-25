b3131dd0b3d5b2065ebedc91e415ae81835203d6
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-25 23:43:25 +0600

    calculated cart cost

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import "./Cart.css";
 
 const Cart = ({cart}) => {
+    //calculate total price
+    const total = cart.reduce((prev, curr) => prev + curr.price ,0);
+    //calculate total shipping charge
+    const totalShippingCharge = cart.reduce((prev, curr) => prev + curr.shipping, 0);
+
+    //Total tax
+    const totalTax = cart.reduce((prev, curr)=> prev + curr.price * 0.1, 0);
+
+    //grand total
+    const grandTotal = total + totalShippingCharge + totalTax;
+
     return (
-        <div>
-            <h1>Order summury</h1>
-            <h4>Total items: {cart.length}</h4>
+        <div className='cart'>
+            <div className='cart-title'>
+                <h4>Order Summury</h4>
+            </div>
+            <div className='cart-calculation'>
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charges: ${totalShippingCharge}</p>
+                <p>Tax: ${parseFloat(totalTax).toFixed(2)}</p>
+                <h4>Grand Total: {grandTotal}</h4>
+            </div>
         </div>
     );
 };
